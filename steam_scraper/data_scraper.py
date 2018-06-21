f2aca191dc4184cd8f1c4b3d1f3dad06040143b6
john <jdqwertqwert@gmail.com>
2018-06-22 01:06:26 +0200

    fixed bug

--- steam_scraper/data_scraper.py
+++ steam_scraper/data_scraper.py
@@ -97,11 +97,11 @@ class Data_Scraper:
                     break
 
         def save_to_float(_str):
-            _str = re.sub(re.compile("\D"), "", _str)
+            _str = re.sub(re.compile("\D"), "", str(_str))
             if _str == "":
                 return float(0)
             else:
-                return float(str(_str))
+                return float(_str)
 
         for result in results_list:
             cont = result.find('div', {'class': 'col search_price discounted responsive_secondrow'})
